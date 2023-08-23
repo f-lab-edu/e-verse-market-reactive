7bbcf42038ff07eca44d536a7d30483674d9e7d1
Seon1134 <jseon382@gmail.com>
2023-08-23 18:19:15 +0900

    feature: init project

---
+++ src/main/kotlin/com/seonjoo/eversemarket/EVerseMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.seonjoo.eversemarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EVerseMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<EVerseMarketApplication>(*args)
+}
